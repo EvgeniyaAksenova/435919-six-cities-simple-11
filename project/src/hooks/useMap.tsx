58419f3949d07fdc9fd9554edcec470c7855ed58
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-11-30 10:58:46 +0300

    +

--- project/src/hooks/useMap.tsx
+++ project/src/hooks/useMap.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {useEffect, useState, MutableRefObject, useRef} from 'react';
 //import {Map, TileLayer} from 'leaflet';
 import leaflet from 'leaflet';
